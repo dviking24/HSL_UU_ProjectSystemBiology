4e99d690f04cbd6881029845dee9d42c1d51aae0
d_viking <antonio.rijnders.name@gmail.com>
2026-05-28 13:47:25 +0200

    Also added 2 new matrices for the ligands & receptors for the vitro-dataset: - df_blastovitro_liganden - df_blastovitro_receptors
    Closes Issue #2

--- Ligands&Receptors.R
+++ Ligands&Receptors.R
@@ -17,6 +17,7 @@ receptors <- unique(data_ligrecep$receptor_ensembl)
 ligreceptors <- intersect(liganden, receptors)
 genen_waar_we_naar_zoeken <- c(liganden, receptors)
 alle_genen_in_vivo <- row.names(data_blastovivo)
+alle_genen_in_vitro <- row.names(data_blastovitro)
 alle_genen_in_endom <- row.names(data_endom)
 
 
@@ -26,6 +27,14 @@ goede_genen_in_vivo_data <- setdiff(alle_genen_in_vivo, onnodige_genen_in_vivo_d
 
 # Blastovivo-dataframe filteren zodat het alleen nog maar rijen met goede genen overhoudt
 data_blastovivo2 <- data_blastovivo[goede_genen_in_vivo_data, ]
+
+
+# Filteren van genen in blastovitro-dataset
+onnodige_genen_in_vitro_data <- setdiff(alle_genen_in_vitro, genen_waar_we_naar_zoeken)
+goede_genen_in_vitro_data <- setdiff(alle_genen_in_vitro, onnodige_genen_in_vitro_data)
+
+# Blastovitro-dataframe filteren zodat het alleen nog maar rijen met goede genen overhoudt
+data_blastovitro2 <- data_blastovitro[goede_genen_in_vitro_data, ]
 
 
 # Filteren van genen in endom-dataset
@@ -44,6 +53,16 @@ ligreceptors_vivo <- intersect(goede_genen_in_vivo_data, ligreceptors)
 df_blastovivo_liganden <- data_blastovivo2[liganden_vivo, ]
 df_blastovivo_receptors <- data_blastovivo2[receptors_vivo, ]
 df_blastovivo_ligreceptors <- data_blastovivo2[ligreceptors_vivo, ]
+
+
+# Dataframes van Liganden, Receptors en LigandReceptor genen in Vitro-dataset
+liganden_vitro <- intersect(goede_genen_in_vitro_data, liganden)
+receptors_vitro <- intersect(goede_genen_in_vitro_data, receptors)
+ligreceptors_vitro <- intersect(goede_genen_in_vitro_data, ligreceptors)
+
+df_blastovitro_liganden <- data_blastovitro2[liganden_vitro, ]
+df_blastovitro_receptors <- data_blastovitro2[receptors_vitro, ]
+df_blastovitro_ligreceptors <- data_blastovitro2[ligreceptors_vitro, ]
 
 
 # Dataframes van Liganden, Receptors en LigandReceptor genen in Endometrium-dataset
