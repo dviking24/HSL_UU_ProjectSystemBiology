51f76e25ad3991761abc96b397f3835cc932fffe
d_viking <antonio.rijnders.name@gmail.com>
2026-06-03 20:01:42 +0200

    Added necessary library imports and the matrices-data now gets loaded.
    Related to Issue #6

--- randomforest.R
+++ randomforest.R
@@ -1,11 +1,8 @@
+#install.packages("caret")
+#install.packages("ranger")
+library(caret)
+library(ranger)
 
-
-
-
-
-
-
-
-
-
-
+# load data
+vivo_matrix <- read.csv("matrices/vivo_matrix.tsv", header = TRUE, sep = "\t", row.names = 1)
+vitro_matrix <- read.csv("matrices/vitro_matrix.tsv", header = TRUE, sep = "\t", row.names = 1)
