4c40d887fcc13925141f2efb9c5b6603924fdea8
d_viking <antonio.rijnders.name@gmail.com>
2026-06-07 15:33:23 +0200

    Added data-loading and train/test-split logic from 'randomforest'-branch
    Related to Issue #21

--- knnclassifier.R
+++ knnclassifier.R
@@ -1,3 +1,57 @@
 library(class)
 library(caret)
 library(ggplot2)
+
+# load data
+vivo_matrix <- read.csv("matrices/vivo_matrix.tsv", header = TRUE, sep = "\t", row.names = 1)
+vitro_matrix <- read.csv("matrices/vitro_matrix.tsv", header = TRUE, sep = "\t", row.names = 1)
+
+
+# transpose data.frames
+vivo_matrix <- as.data.frame(t(vivo_matrix))
+vitro_matrix <- as.data.frame(t(vitro_matrix))
+
+
+# add 'PregnancyStatus'-column to data.frames, and automatically fill column for every row
+# vivo
+rn <- rownames(vivo_matrix)
+
+vivo_matrix2 <- cbind(
+  PregnancyStatus = ifelse(
+    grepl("NP", rn),
+    "NOT_PREGNANT",
+    "PREGNANT"
+  ),
+  vivo_matrix
+)
+
+# vitro
+rn <- rownames(vitro_matrix)
+
+vitro_matrix2 <- cbind(
+  PregnancyStatus = ifelse(
+    grepl("NP", rn),
+    "NOT_PREGNANT",
+    "PREGNANT"
+  ),
+  vitro_matrix
+)
+
+
+# train-test split
+set.seed(64) # setting seed
+
+# vivo
+vivo_matrix2$PregnancyStatus <- factor(vivo_matrix2$PregnancyStatus) # converts 'PregnancyStatus' target variable to a factor for classification
+
+vivo_train_index <- createDataPartition(vivo_matrix2$PregnancyStatus, p = 0.8, list = FALSE) # 80/20 split
+vivo_training_set <- vivo_matrix2[vivo_train_index, ]
+vivo_test_set <- vivo_matrix2[-vivo_train_index, ]
+
+# vitro
+vitro_matrix2$PregnancyStatus <- factor(vitro_matrix2$PregnancyStatus) # converts 'PregnancyStatus' target variable to a factor for classification
+
+vitro_train_index <- createDataPartition(vitro_matrix2$PregnancyStatus, p = 0.8, list = FALSE) # 80/20 split
+vitro_training_set <- vitro_matrix2[vitro_train_index, ]
+vitro_test_set <- vitro_matrix2[-vitro_train_index, ]
+
